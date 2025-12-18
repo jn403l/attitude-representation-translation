06d48256102b44770d46e8f5ac8968195945942c
jn403l <jnmbanta@gmail.com>
2025-12-18 14:04:23 +0900

    added a simple example in example/main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,4 +1,19 @@
-int main()
+#include <string>
+#include <iostream>
+#include <cmath>
+
+#include "AttitudeMathLib.h"
+
+using namespace AML;
+
+int main(int argc, char **argv)
 {
+	Vector3 v1(1.0, 2.0, 3.0);
+	Vector3 v2(4.0, 5.0, 6.0);
+
+	Vector3 v3 = v1 + v2;
+	std::cout << v1 << std::endl;
+	std::cout << v2 << std::endl;
+	std::cout << v3 << std::endl;
   return 0;
 }
